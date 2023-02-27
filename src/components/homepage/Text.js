4d8a98d979c80cf3ffb50eb8ea71c90ca4a75b4f
mia-926 <75146430+mia-926@users.noreply.github.com>
2023-02-26 19:13:16 -0800

    made more dynamic
     circles move with page and text moves with page and image moves up for phone size

--- src/components/homepage/Text.js
+++ src/components/homepage/Text.js
@@ -1,13 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import "./homepage.css";
+import "./textStyle.css";
+
 
 export const Text = () => {
 
-    const [windowWidth, setwindowWidth] = useState(window.innerWidth)
+    const startWidth = () => {
+        if (window.innerWidth > 990){
+            return(window.innerWidth *.6)
+        }
+        else{
+            return(window.innerWidth)
+        }
+    }
+
+    const startHeight = () => {
+        if (window.innerWidth > 990){
+            return(window.innerHeight *.35)
+        }
+        else{
+            return(window.innerHeight *.5)
+        }
+    }
+
+    const [windowWidth, setwindowWidth] = useState(()=>startWidth())
+    const [windowHeight, setwindowHeight] = useState(()=>startHeight())
 
 
     const handleResize = () => {
-        setwindowWidth(window.innerWidth)
+        if (window.innerWidth > 990){
+            setwindowWidth(window.innerWidth *.6)
+        }
+        else{
+            setwindowWidth(window.innerWidth)
+        }
     }
 
     useEffect(() =>{
@@ -18,10 +44,9 @@ export const Text = () => {
         }
     }, [])
 
-
     return(
-        <div style = {{display: 'flex', flex: 1, alignItems: 'center', flexDirection: 'column', position: 'absolute', top: window.innerHeight*.35, backgroundColor: 'white', width: windowWidth*.6}}>
-            <h2  style = {{flex: 2}}>Free Group Tennis Lessons In Westlake Village</h2>
+        <div className = "firstDiv" style = {{top: windowHeight, width: windowWidth}}>
+            <h2  style = {{flex: 2, fontSize: 24}}>Free Group Tennis Lessons In Westlake Village</h2>
             <div style={{flexDirection: 'row', paddingTop: 40}}>
                 <a style = {{marginRight: 10, width: 120, paddingTop: 8, paddingBottom: 8}}class="greenb btn btn-primary" href="join" role="button">Join a Lesson</a>
                 <a style = {{marginLeft: 10, width: 120, paddingTop: 20, paddingBottom: 20}} class="whiteb btn btn-outline-primary" href="learn" role="button">Learn More</a>
