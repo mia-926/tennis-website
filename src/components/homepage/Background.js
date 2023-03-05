5dfd8b1eb9480208bce2bfed264e751b7e828635
mia-926 <75146430+mia-926@users.noreply.github.com>
2023-03-04 17:48:07 -0800

    added footer

--- src/components/homepage/Background.js
+++ src/components/homepage/Background.js
@@ -3,6 +3,7 @@ import "./homepage.css";
 import backgroundPic from '../../images/croppedSerena.png';
 import widePic from '../../images/wideView.png';
 import { Circles } from './Circles';
+import { Text } from './Text';
 
 export const Background = () => {
     const position = () => {
@@ -57,9 +58,11 @@ export const Background = () => {
         }
     }, [])
 
-    return(<div>
+    return(<div style={{height: imageHeight, background:"white"}}><div>
         <img style={{height: imageHeight, width:imageWidth, float: imagePosition}} src = {imageURL} alt="background"/>
-
+        </div>
+        <Circles/>
+        <Text/>
         </div>
     );
 }
--- src/components/homepage/Home.js
+++ src/components/homepage/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { MyCarousel } from '../calendar/MyCarousel';
+import { Footer } from '../Footer';
 import { MyNavBar } from '../navBar/MyNavBar';
 import { Background } from './Background';
 import { Circles } from './Circles';
@@ -7,9 +9,15 @@ import { Text } from './Text';
 export const Home = () => {
   return (
     <div>
+      <div>
       <Background/>
-      <Circles/>
-      <Text/>
+      </div>
+      <div>
+      <h2 style={{background: "#294539", margin:0, padding:0}}>
+      </h2>
+      {/* <MyCarousel/> */}
+      </div>
+    <Footer/>
     </div>
   );
 }
