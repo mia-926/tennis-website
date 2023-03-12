50af45b644de4701198747ddfbc40147966d3dbd
mia-926 <75146430+mia-926@users.noreply.github.com>
2023-03-12 16:14:06 -0700

    added learn more

--- src/components/navBar/LeftNav.js
+++ src/components/navBar/LeftNav.js
@@ -13,7 +13,7 @@ function BootstrapNav(props) {
       <Nav.Link  style={{color:myColor}} href="/instructors">Instructors</Nav.Link>
       <Nav.Link  style={{color:myColor}} href="/mission">Mission</Nav.Link>
       <Nav.Link  style={{color:myColor}} href="/join-lesson">Join a lesson</Nav.Link>
-      
+      <Nav.Link  style={{color:myColor}} href="/learn-more">Learn More</Nav.Link>
       <Nav.Link  style={{color:myColor}} href="/account">Account</Nav.Link>
      <Nav.Link  style={{color:myColor}} href="/admin-page">Admin</Nav.Link>
     </Nav>
