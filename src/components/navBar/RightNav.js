0c1b52e7ed45a5f46023026727c9f02f6e457ad7
Jai Garg <jsgarg314@gmail.com>
2023-03-14 11:40:11 -0700

    fixed admin

--- src/components/navBar/RightNav.js
+++ src/components/navBar/RightNav.js
@@ -12,10 +12,18 @@ function BootstrapNav(props) {
     setAuth({})
     window.location.href = "/home";
   }
-  if(auth !== undefined && auth?._id !== undefined){
+  if(auth !== undefined && auth?._id !== undefined  && auth?.admin === true){
     return (
-          <Nav style={{ backgroundColor:'transparent', borderRadius:"0 0 0 15px", backgroundColor:background}}>
-              <Nav.Link  style={{color:myColor, fontWeight:800, fontSize:'90%'}} href="/admin-page">Admin</Nav.Link>
+      <Nav style={{ backgroundColor:'transparent', borderRadius:"15px 0 0 15px", backgroundColor:background}}>
+          <Nav.Link  style={{color:myColor, fontWeight:800, fontSize:'90%'}} href="/admin-page">Admin</Nav.Link>
+          <Nav.Link  style={{color:myColor, fontWeight:800, fontSize:'90%'}}className = "white" href="/create-account">Create Account</Nav.Link>
+          <Nav.Link style={{color:myColor, fontWeight:800, fontSize:'90%'}} className = "white" onClick = {signOut}>Sign Out</Nav.Link>
+      </Nav>
+);
+}
+  if(auth === undefined || auth?._id === undefined){
+    return (
+          <Nav style={{ backgroundColor:'transparent', borderRadius:"15px 0 0 15px", backgroundColor:background}}>
               <Nav.Link  style={{color:myColor, fontWeight:800, fontSize:'90%'}}className = "white" href="/create-account">Create Account</Nav.Link>
               <Nav.Link  style={{color:myColor, fontWeight:800, fontSize:'90%'}}className = "white" href="/login">Login</Nav.Link>
           </Nav>
